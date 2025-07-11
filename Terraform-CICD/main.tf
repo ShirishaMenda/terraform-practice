c89dd02ccd9284744f90c12b977a65f69a65fa01
ShirishaMenda <157020549+ShirishaMenda@users.noreply.github.com>
2025-07-11 19:37:36 +0530

    Update main.tf

--- Terraform-CICD/main.tf
+++ Terraform-CICD/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-northeast-1"
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-04b4f1a9cf54c11d0"
+    ami = "ami-03598bf9d15814511"
     instance_type = "t2.micro"
     tags = {
       Name = "test"
